fd04a24e87e6a032280cc160cf8167a1aad81dcc
victor <nwaigwevic25@gmail.com>
2024-12-18 21:42:26 +0700

    Initial commit for Heroku deployment

---
+++ textcount/text/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+  return render(request, 'index.html')
+
+def counter(request):
+  words = request.POST['words']
+  amount_of_words = len(words.split())
+  return render(request,'counter.html', {'amount': amount_of_words})
+
